2e8617482dc41cc59b563b750df45b378d590e44
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2025-08-20 11:38:17 +0200

    fix: Fix eslint plugin typing again

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import type { TSESLint } from '@typescript-eslint/utils';
 import { rule as servicePropertyOrder } from './service-property-order.js';
 import { rule as noPublishedWorkers } from './no-published-workers.js';
 
@@ -8,29 +9,29 @@ const pkg = JSON.parse(
 
 const namespace = '@treatwell/moleculer';
 
-export const meta = {
+const meta = {
   name: pkg.name,
   version: pkg.version,
   namespace,
 };
 
-export const rules = {
+const rules = {
   'service-property-order': servicePropertyOrder,
   'no-published-workers': noPublishedWorkers,
+};
+
+const recommended: TSESLint.FlatConfig.Config = {
+  plugins: { [namespace]: { meta, rules } },
+  rules: {
+    [`${namespace}/service-property-order`]: 'error',
+    [`${namespace}/no-published-workers`]: 'error',
+  },
 };
 
 const plugin = {
   meta,
   rules,
-  configs: {
-    recommended: {
-      plugins: { [namespace]: { meta, rules } },
-      rules: {
-        [`${namespace}/service-property-order`]: 'error',
-        [`${namespace}/no-published-workers`]: 'error',
-      },
-    },
-  },
+  configs: { recommended },
 };
 
 export default plugin;
