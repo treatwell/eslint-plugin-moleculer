ee0577cf9c6a6d3be3e5a7c68112c23a60991912
Marc-Antoine <marcantoinefernandes@gmail.com>
2025-08-20 11:29:51 +0200

    fix: Correctly type plugin configs to be usable (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,34 @@
 import fs from 'node:fs';
-import type { TSESLint } from '@typescript-eslint/utils';
-import { rule } from './service-property-order.js';
+import { rule as servicePropertyOrder } from './service-property-order.js';
 import { rule as noPublishedWorkers } from './no-published-workers.js';
 
 const pkg = JSON.parse(
-  fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8'),
+  fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'),
 );
 
 const namespace = '@treatwell/moleculer';
 
-const plugin: TSESLint.Linter.Plugin = {
-  meta: {
-    name: pkg.name,
-    version: pkg.version,
-  },
-  rules: {
-    'service-property-order': rule,
-    'no-published-workers': noPublishedWorkers,
-  },
+export const meta = {
+  name: pkg.name,
+  version: pkg.version,
+  namespace,
 };
 
-plugin.configs = {
-  recommended: {
-    plugins: {
-      // @ts-expect-error ts eslint doesn't support this syntax yet but is
-      // the recommended way to define plugins per eslint docs
-      [namespace]: plugin,
-    },
-    rules: {
-      [`${namespace}/service-property-order`]: 'error',
-      [`${namespace}/no-published-workers`]: 'error',
+export const rules = {
+  'service-property-order': servicePropertyOrder,
+  'no-published-workers': noPublishedWorkers,
+};
+
+const plugin = {
+  meta,
+  rules,
+  configs: {
+    recommended: {
+      plugins: { [namespace]: { meta, rules } },
+      rules: {
+        [`${namespace}/service-property-order`]: 'error',
+        [`${namespace}/no-published-workers`]: 'error',
+      },
     },
   },
 };
